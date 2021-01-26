33bc3e96fd334581be813c43694c64aa014cd3db
OA Wu <comdan66@gmail.com>
2021-01-27 01:08:50 +0800

    fix code

---
+++ src/clock.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 - 2021, LaliloCore
+ * @license     http://opensource.org/licenses/MIT  MIT License
+ * @link        https://www.ioa.tw/
+ */
+
+namespace HTML;
+
+echo html(
+  head(
+    meta()->attr('http-equiv', 'Content-Language')->content('zh-tw'),
+    meta()->attr('http-equiv', 'Content-type')->content('text/html; charset=utf-8'),
+    meta()->name('viewport')->content('width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui'),
+
+    title('⏰ 時鐘 滴滴噠噠'),
+
+    asset()
+      ->css('icon.css')
+      ->css('clock.css')
+      ->js('https://code.jquery.com/jquery-1.12.4.min.js')
+      ->js('https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js')
+      ->js('core.js')
+      ->js('clock.js')
+  ),
+  body()
+)->lang('zh-Hant');
